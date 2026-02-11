f1c8a9d2b64dead0695bf7b5a0ab8059bf781743
PC-ALTERNANT-IT\timou <figard.tim@gmail.com>
2026-02-11 08:25:26 +0100

    Inscription non finit

---
+++ Controllers/InscriptionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Thiskord_Back.Services;
+using System.Text.Json;
+
+namespace Thiskord_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class   InscriptionController : Controller
+    {
+        private readonly InscriptionService _inscriptionService;
+        private readonly LogService _logService;
+
+        public InscriptionController(InscriptionService inscriptionService, LogService logService)
+        {
+            _inscriptionService = inscriptionService;
+            _logService = logService;
+        }
+
+
+                }
+            }
---
+++ Services/InscriptionService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Thiskord_Back.Services
+{
+    public class InscriptionService
+    {
+        private readonly IDbConnectionService _dbService;
+        private readonly LogService _logService;
+
+        public InscriptionService(IDbConnectionService dbService, LogService logService)
+        {
+            _dbService = dbService;
+            _logService = logService;
+        }
+
+        public async Task<int> InscriptionUser(string user_name, string user_password)
+        {
+            
+        }   
